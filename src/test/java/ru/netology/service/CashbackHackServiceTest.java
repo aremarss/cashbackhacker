b3f79764f3b7e89985955b2edc9fbb4f697314ec
aremarss <aremars@mail.ru>
2021-12-27 08:09:34 +0300

    edit tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -34,6 +33,36 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldReturn0Rub() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn100RubJupiter() {
+        assertEquals(service.remain(900), 100);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn999RubJupiter() {
+        assertEquals(service.remain(1), 999);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn1RubJupiter() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn1000RubJupiter() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn500RubJupiter() {
+        assertEquals(service.remain(500), 500);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn0RubJupiter() {
         assertEquals(service.remain(1000), 0);
     }
 }
